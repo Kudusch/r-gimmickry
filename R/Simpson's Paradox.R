983cc013ffdd8a6ef74202d94c68b857c4995de7
Kudusch <kudusch@posteo.de>
2018-09-07 16:31:37 +0200

    Improve data structure and add ggplot2

--- R/Simpson's Paradox.R
+++ R/Simpson's Paradox.R
@@ -1,36 +1,45 @@
 # Simpson's Paradox
 
-# Set up vars
-experiment <- c()
-experiment_one <- c()
-experiment_two <- c()
+# Set up config
+randomness <- 80
 
 # Generate a series of measurements of interval data in two experiments
 # Experiment one
+a <- c()
 for(i in 100:1) {
   n <- i*1.5
-  experiment_one <- c(experiment_one, n+runif(1,-80,80))
+  a <- c(a, n+runif(1,randomness*-1,randomness))
 }
+a <- data.frame(a,'a')
+names(a) <- c('value', 'experiment')
 # Experiment two
-for(i in 300:201) {
+b <- c()
+for(i in ((300):(201))) {
   n <- i*1.5
-  experiment_two <- c(experiment_two, n+runif(1,-80,80))
+  b <- c(b, n+runif(1,randomness*-1,randomness))
 }
+b <- data.frame(b,'b')
+names(b) <- c('value', 'experiment')
 # Combine the results of both experiments
-experiment <- c(experiment_one,experiment_two)
+df <- rbind(a, b)
 
 # Fit and test combined experiments
 # Result: There is a significant, positive relationship
-fitAll<- lm(1:length(experiment)~experiment);fitAll
-cor.test(1:length(experiment),experiment)
+fitAll<- lm(1:length(df$value)~df$value);fitAll
+cor.test(1:length(df$value), df$value)
 
 # Fit experiment one and two
-fitOne <- lm(1:length(experiment_one)~experiment_one)
-fitTwo <- lm(1:length(experiment_two)~experiment_two)
+fitA <- lm(1:length(subset(df, df$experiment == 'a')$value)~subset(df, df$experiment == 'a')$value); fitA
+fitB <- lm(1:length(subset(df, df$experiment == 'b')$value)~subset(df, df$experiment == 'b')$value); fitB
 
 # Plot to show that the positive relationship only exists between the groups, 
 # not within the groups
-plot(1:length(experiment)~experiment)
-abline(fitAll, col="blue")
-abline(fitOne, col="red")
-abline(fitTwo$coefficients[1]+100,fitTwo$coefficients[2], col="red")
+library(ggplot2)
+ggplot(df, aes(x=1:length(df$value), y=df$value, color = df$experiment)) +
+  geom_point(shape=1) +
+  geom_smooth(method='lm') +
+  geom_abline(intercept = fitAll$coefficients[1]) +
+  labs(title = "", y = "", x = "", color = "Experiments")
+
+# Clean up
+rm(a, b, df, fitA, fitAll, fitB, i, randomness, n)
